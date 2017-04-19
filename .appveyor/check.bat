6a6a5f71dce246bdd72e22f7b05a421c2d1945f8
Christoph Auer <auer.chrisi@gmx.net>
2017-04-19 10:56:50 +0200

    asd

--- .appveyor/check.bat
+++ .appveyor/check.bat
@@ -1,9 +1,18 @@
 @echo off
 
-call ".\common.bat"
+call:checkCommitMessage
+GOTO:EOF
 
 :checkCommitMessage
-    call:logInfo "Commit message: ${%APPVEYOR_REPO_COMMIT_MESSAGE%}"
-    call:logInfo "Extended commit message: ${%APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED%}"
+    call:logInfo "Commit message: %APPVEYOR_REPO_COMMIT_MESSAGE%"
+    call:logInfo "Extended commit message: %APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED%"
     call:logInfo "Is pull request: ${TRAVIS_PULL_REQUEST}"
+GOTO:EOF
+
+:logInfo
+    echo [INFO] %~1
+GOTO:EOF
+
+:logError
+    echo [ERROR] %~1
 GOTO:EOF
